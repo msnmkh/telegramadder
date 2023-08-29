15f240ac135827af5fbaeda27a3ecfa3623468bf
Mohsen Mokhtari <wav1376@gmail.com>
2023-08-29 19:16:54 +0330

    Create CONSTANT.py

---
+++ CONSTANT.py
@@ -0,0 +1,17 @@
+# Listed by Telegram Downloads During 2022
+COUNTRIES = {
+    "1"   : {"Name":"India","Code":"+91"},
+    "2"   : {"Name":"Russia","Code":"+7"},
+    "3"   : {"Name":"UnitedStates","Code":"+1"},
+    "4"   : {"Name":"Indonesia","Code":"+62"},
+    "5"   : {"Name":"Brazil","Code":"+55"},
+    "6"   : {"Name":"Egypt","Code":"+20"},
+    "7"   : {"Name":"Turkey","Code":"+90"},
+    "8"   : {"Name":"Iran","Code":"+98"},
+    "9"   : {"Name":"France","Code":"+33"},
+}
+
+CSVLINK = ""
+GROUPID = 0
+API_ID = 0
+API_HASH = ''
